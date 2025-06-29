0409518a9a7d0090258aada0d18b24808f1a5a82
covearchitects <uma.covearch@gmail.com>
2025-06-29 13:49:11 +0530

    Delete src directory

--- src/App.jsx
+++
@@ -1,7 +0,0 @@
-export default function App() {
-  return (
-    <div className="min-h-screen flex items-center justify-center bg-red-100">
-      <h1 className="text-4xl font-bold text-center">Welcome to COVE Architects</h1>
-    </div>
-  )
-}
